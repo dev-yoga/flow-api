561642eebcb97f423214d72eba49494c274dc95c
dev-yoga <spin_shoppe@icloud.com>
2021-10-09 19:48:19 -0700

    Added update and delete and it works yay :)

--- src/controllers/pose.controller.js
+++ src/controllers/pose.controller.js
@@ -1,21 +1,58 @@
 import lodash from 'lodash';
-import Post from '../models/post.js';
+import Pose from '../models/pose.js';
 
 const { get } = lodash;
 
+// create / POST
 export async function createPoseHandler(req, res) {
     const body = req.body;
-    await Post.create(body);
+    await Pose.create(body);
     return res.sendStatus(201);
 }
 
+// read / GET
 export async function getPoseHandler(req, res) {
-    const postId = get(req, 'params.poseId');
-    const post = await Post.findById(postId);
-    return res.send(post);
+    const poseId = get(req, 'params.poseId');
+    const pose = await Pose.findById(poseId);
+
+    if (!pose) {
+        return res.sendStatus(404);
+    }
+    
+    return res.send(pose);
 }
 
 export async function listPoseHandler(req, res) {
-    const posts = await Post.find({});
-    return res.send(posts);
+    const poses = await Pose.find({});
+    return res.send(poses);
+}
+
+// update / PUT
+
+export async function updatePoseHandler(req, res) {
+    const poseId = await get(req, 'params.poseId');
+    const update = req.body;
+
+    const pose = await Pose.findById(poseId);
+
+    if (!pose) {
+        return res.sendStatus(404);
+    }
+
+    const updatedPose = await Pose.findOneAndUpdate({ poseId }, update, { new:true });
+
+    return res.send(updatedPose);
+}
+
+// delete / DELETE
+
+export async function deletePoseHandler(req, res) {
+    const poseId = get(req, 'params.poseId');
+    const pose = await Pose.findByIdAndDelete(poseId);
+
+    if (!pose) {
+        return res.sendStatus(404);
+    }
+
+    return res.sendStatus(200);
 }
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
-import { createPoseHandler, getPoseHandler, listPoseHandler } from './controllers/pose.controller.js';
+import { updatePoseHandler, createPoseHandler, getPoseHandler, listPoseHandler, deletePoseHandler } from './controllers/pose.controller.js';
 
 export default function(app) {
     app.get('/healthcheck', (req, res) => res.sendStatus(200));
     app.get('/api/pose/:poseId', getPoseHandler);
     app.post('/api/pose', createPoseHandler);
     app.get('/api/pose/', listPoseHandler);
+    app.put('/api/pose/:poseId', updatePoseHandler);
+    app.delete('/api/pose/:poseId', deletePoseHandler);
 }
