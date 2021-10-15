5e44dd4a025d316a906a4ef957004757a4e25583
dev-yoga <spin_shoppe@icloud.com>
2021-10-14 18:42:41 -0700

    app.js auth

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
+import dotenv from 'dotenv';
 import express from 'express';
-// import config from '../config/config.js';
+import passport from 'passport';
 import connect from './db/connect.js';
 import routes from './routes.js';
-import dotenv from 'dotenv';
+import verifyJwt from './auth/verify-jwt.js';
+
+
 
 dotenv.config();
 
-const port = process.env.port;
-const host = process.env.host;
+passport.use(verifyJwt());
 
 const app = express();
-
+app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+const port = process.API_PORT;
+const host = process.API_HOST;
 app.listen(port, host, () => {
     connect();
     routes(app);
