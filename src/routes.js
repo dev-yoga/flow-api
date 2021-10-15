0b4a6f09dd90f97e4f2e5abbd0b451d47d5881d3
dev-yoga <spin_shoppe@icloud.com>
2021-10-14 19:50:05 -0700

    Routes are secure

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import passport from 'passport';
 import { searchPoseHandler, updatePoseHandler, createPoseHandler, getPoseHandler, listPoseHandler, deletePoseHandler } from './controllers/pose.controller.js';
 import { createSequenceHandler, deleteSequenceHandler, getSequenceHandler, listSequenceHandler, updateSequenceHandler } from './controllers/sequence.controller.js';
 import { signUp, signIn } from './controllers/user.controller.js';
@@ -6,17 +7,17 @@ export default function(app) {
     app.get('/healthcheck', (req, res) => res.sendStatus(200));
     app.post('/api/signup', signUp);
     app.post('/api/signin', signIn);
-    app.get('/api/pose/:poseId', getPoseHandler);
-    app.post('/api/pose', createPoseHandler);
-    app.get('/api/pose/', listPoseHandler);
-    app.put('/api/pose/:poseId', updatePoseHandler);
-    app.delete('/api/pose/:poseId', deletePoseHandler);
-    app.get('/api/sequence/:sequenceId', getSequenceHandler);
-    app.get('/api/sequence', listSequenceHandler);
-    app.post('/api/sequence', createSequenceHandler);
-    app.put('/api/sequence/:sequenceId', updateSequenceHandler);
-    app.delete('/api/sequence/:sequenceId', deleteSequenceHandler);
-    app.get('/api/pose/search/:terms', searchPoseHandler);
+    app.get('/api/pose/:poseId', passport.authenticate('jwt', { session: false }), getPoseHandler);
+    app.post('/api/pose', passport.authenticate('jwt', { session: false }), createPoseHandler);
+    app.get('/api/pose/', passport.authenticate('jwt', { session: false }), listPoseHandler);
+    app.put('/api/pose/:poseId', passport.authenticate('jwt', { session: false }), updatePoseHandler);
+    app.delete('/api/pose/:poseId', passport.authenticate('jwt', { session: false }), deletePoseHandler);
+    app.get('/api/sequence/:sequenceId', passport.authenticate('jwt', { session: false }), getSequenceHandler);
+    app.get('/api/sequence', passport.authenticate('jwt', { session: false }), listSequenceHandler);
+    app.post('/api/sequence', passport.authenticate('jwt', { session: false }), createSequenceHandler);
+    app.put('/api/sequence/:sequenceId', passport.authenticate('jwt', { session: false }), updateSequenceHandler);
+    app.delete('/api/sequence/:sequenceId', passport.authenticate('jwt', { session: false }), deleteSequenceHandler);
+    app.get('/api/pose/search/:terms', passport.authenticate('jwt', { session: false }), searchPoseHandler);
 
 }
 
